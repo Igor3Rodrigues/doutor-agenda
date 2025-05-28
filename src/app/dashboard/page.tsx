ce44086d2e140baadc6e025d9ffcdcfed417c945
Igor <igor3lima@gmail.com>
2025-05-28 12:27:16 -0300

    feat: adicionar dependências do Radix UI e implementar verificação de clínicas no dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,9 @@ import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { usersToClinicsTable } from "@/db/schema";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -11,6 +14,13 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+  // Preciso pegar as clinicas do usuario
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic_form");
   }
 
   return (
