ea820b1f5a2177d989a0b8820c0cc2e7b6fd7fd9
Igor <igor3lima@gmail.com>
2025-05-28 12:56:11 -0300

    feat: implement login with Google and improve error messages on login form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,12 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
-import { redirect } from "next/navigation";
-import { db } from "@/db";
-import { eq } from "drizzle-orm";
-import { usersToClinicsTable } from "@/db/schema";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -15,14 +15,12 @@ const DashboardPage = async () => {
   if (!session?.user) {
     redirect("/authentication");
   }
-  // Preciso pegar as clinicas do usuario
   const clinics = await db.query.usersToClinicsTable.findMany({
     where: eq(usersToClinicsTable.userId, session.user.id),
   });
   if (clinics.length === 0) {
-    redirect("/clinic_form");
+    redirect("/clinic-form");
   }
-
   return (
     <div>
       <h1>Dashboard</h1>
