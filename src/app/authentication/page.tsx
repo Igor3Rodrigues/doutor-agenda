51d72dffcc320be53cf38f65882a267fb5d83ec2
Igor <igor3lima@gmail.com>
2025-05-28 11:20:32 -0300

    feat: implementar verificação de sessão na página de autenticação e no dashboard

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,11 +1,19 @@
-"use client";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { auth } from "@/lib/auth";
 
 import LoginForm from "./components/login-up-forms";
 import SignUpForm from "./components/sign-up-forms";
 
-const AuthenticationPage = () => {
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
